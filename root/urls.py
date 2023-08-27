1ffdd0d38be541943be5e6b123e25370abf322f9
yac72mh <mehrabi.yac@gmail.com>
2023-08-27 05:53:08 -0700

    234

--- root/urls.py
+++ root/urls.py
@@ -1,8 +1,10 @@
-from django.contrib import admin
 from django.urls import path , include
 from .views import *
+
+
 app_name = 'root'
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path ("" , home , name="home"),
+    path ("about" , about , name="about"),
+    path ("contact" , contact , name="contact"),
 ]
